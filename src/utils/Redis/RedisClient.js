18576d82da10feea6240daec2273bf9e191a9b12
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2026-06-19 03:09:07 +0300

    Update RedisClient.js

--- src/utils/Redis/RedisClient.js
+++ src/utils/Redis/RedisClient.js
@@ -1,17 +1,30 @@
 import { createClient } from 'redis';
 
-const redisClient = createClient({ url: process.env.RedisURL || "redis://127.0.0.1:6379" });
 
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
+const redisClient = createClient({
+    url: process.env.RedisURL,
+    socket: {
+        reconnectStrategy: (retries) => {
+           
+            if (retries > 20) return new Error('Max retries reached');
+            return Math.min(retries * 50, 2000);
+        }
+    }
+});
+
+redisClient.on('error', (err) => console.error('❌ Redis Client Error', err));
+redisClient.on('connect', () => console.log('🚀 Redis client connecting...'));
+redisClient.on('ready', () => console.log('✅ Redis client ready to use!'));
 
 
-(async () => {
+const connectRedis = async () => {
     try {
         await redisClient.connect();
-        console.log('✅ Connected to Redis successfully');
     } catch (error) {
         console.error('❌ Could not connect to Redis', error);
     }
-})();
+};
+
+connectRedis();
 
 export default redisClient;
