4e2a77c302149e20ebbc19dab8dc783d0cca8713
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2026-06-21 02:00:39 +0300

    Update CartValidation.js

--- src/modules/Cart/CartValidation.js
+++ src/modules/Cart/CartValidation.js
@@ -7,7 +7,7 @@ export const addToCartSchema = {
     body: joi.object({
         productId: joi.string().hex().length(24).required(), 
         quantity: joi.number().integer().min(1).required(),   
-        cartId: joi.string().required()                      
+        cartId: joi.string()                      
     }).required()
 }
 
