106bfd06eb2b9b4f1ca0b1f2666cf7808c6a6d46
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2026-06-21 05:13:31 +0300

    Update Product.model.js

--- DB/Models/Product.model.js
+++ DB/Models/Product.model.js
@@ -5,7 +5,7 @@ const ProductSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Category",
   },
-   CategoryId: {
+  CategoryName: {
     type: String,
     ref: "Category",
   },
